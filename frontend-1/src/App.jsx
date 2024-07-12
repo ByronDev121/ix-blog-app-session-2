8f4caf6777cfb6b63dcca1b46ac34acea5a8d897
Byron de Villiers <bdevilliers@shoprite.co.za>
2024-07-12 11:26:29 +0200

    frontend dir fix

---
+++ frontend-1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./App.css";
+
+import HomePage from "./pages/Home";
+import BlogsPage from "./pages/Blogs";
+
+function App() {
+  return <HomePage />;
+  // return <BlogsPage />;
+}
+
+export default App;
